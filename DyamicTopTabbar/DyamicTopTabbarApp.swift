536a3e5f3007ef35458f160b5068d2d6a9602bc1
Muskan Jindal <muskanjindal78@gmail.com>
2023-04-05 08:19:12 +0530

    Initial Commit

---
+++ DyamicTopTabbar/DyamicTopTabbarApp.swift
@@ -0,0 +1,17 @@
+//
+//  DyamicTopTabbarApp.swift
+//  DyamicTopTabbar
+//
+//  Created by head_guest on 05/04/23.
+//
+
+import SwiftUI
+
+@main
+struct DyamicTopTabbarApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
